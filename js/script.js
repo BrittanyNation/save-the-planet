a6bc94ac4f6137e5fb52376898bce07aa18257a8
Brittany Nation <121448506+BrittanyNation@users.noreply.github.com>
2024-07-05 10:01:29 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  $('.amount input').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+});
